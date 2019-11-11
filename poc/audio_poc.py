1a620702c0e44d62f4c8085e146fd64495a10517
Andreas Rebsamen <andreas.rebsamen@bluewin.ch>
2019-11-11 12:39:01 -0500

    add prove of concept for the audio with sonic pi

---
+++ poc/audio_poc.py
@@ -0,0 +1,30 @@
+import pyautogui as pag
+import psonic as ps
+
+maxPitch = 90
+minPitch = 40
+
+maxVol = 4
+minVol = .1
+
+screenSize = pag.size()
+width = screenSize[0]
+height = screenSize[1]
+
+print("making music")
+
+while True:
+    pos = pag.position()
+    x = pos[0]
+    y = pos[1]
+
+    ps.use_synth(ps.DULL_BELL)
+    pitch = x*(maxPitch-minPitch)/width + minPitch
+    vol = y*(maxVol-minVol)/height + minVol
+
+    print (str(x) + " " + str(y) + '\r')
+
+
+    ps.play(pitch, sustain=.05, attack=.05, release=.05, amp=vol)
+    ps.sleep(.05)
+
