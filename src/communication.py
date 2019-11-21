c1165a5ff8edec7019256ebf80e84897319ac9f7
Andreas Rebsamen <andreas.rebsamen@bluewin.ch>
2019-11-21 10:40:43 -0500

    comment and correct host for connect()

--- src/communication.py
+++ src/communication.py
@@ -2,8 +2,8 @@ import socket
 
 class ThereminCommunication:
 
-    UDP_IP_VISUAL = "piar.local"
-    UDP_IP_AUDIO =  "DESKTOP-A4L73LO.local"
+    UDP_IP_VISUAL = "piar.local"            # hostname or ip for visual.py
+    UDP_IP_AUDIO =  "DESKTOP-A4L73LO.local" # hostname or ip for audio.py
     UDP_PORT_PITCH = 5005
     UDP_PORT_VOL = 5006
 
@@ -18,8 +18,8 @@ class ThereminCommunication:
                             socket.SOCK_DGRAM) # UDP
 
     def bind(self):
-        self.pitchSock.bind((self.UDP_IP_VISUAL, self.UDP_PORT_PITCH))
-        self.volSock.bind((self.UDP_IP_VISUAL, self.UDP_PORT_VOL))
+        self.pitchSock.bind((self.UDP_IP_AUDIO, self.UDP_PORT_PITCH))
+        self.volSock.bind((self.UDP_IP_AUDIO, self.UDP_PORT_VOL))
 
     def connect(self):
         self.pitchSock.connect((self.UDP_IP_AUDIO, self.UDP_PORT_PITCH))
