53bc4211ab0831987f8057966256b939017a4c16
Andreas Rebsamen <andreas.rebsamen@bluewin.ch>
2019-11-21 11:21:28 -0500

    add function to crop sections

--- src/box.py
+++ src/box.py
@@ -12,32 +12,38 @@ class box:
         self.space = space
         self.thickness = thickness
 
-    def drawBoxes(self, frame):
         # Top left corner of rectangle
-        start_point1 = (self.space, self.space)
+        self.start_point_vol = (self.space, self.space)
         
         # Bottom right corner of rectangle
-        end_point1 = ( int(self.width/4), (self.height - self.space))
-        
+        self.end_point_vol = ( int(self.width/4), (self.height - self.space))
+      
+
+        # Top left corner of rectangle
+        self.start_point_pitch = (int((self.width/4) + self.space), int(self.height - self.height/2.5))
+
+        # Bottom right corner of rectangle
+        self.end_point_pitch = ( (self.width - self.space), (self.height - self.space))
+
+
+    def drawBoxes(self, frame):
+  
+        ### Makes Vertical Box 
         # Blue color in BGR
         color1 = (255, 0, 0)
-        
-        cv2.rectangle(frame, start_point1, end_point1, color1, self.thickness)
+        cv2.rectangle(frame, self.start_point_vol, self.end_point_vol, color1, self.thickness)
 
-        ### Makes Horizontal Box
-        
-        # Top left corner of rectangle
-        start_point2 = (int((self.width/4) + self.space), int(self.height - self.height/2.5))
-
-        # Bottom right corner of rectangle
-        end_point2 = ( (self.width - self.space), (self.height - self.space))
-
+        ### Makes Horizontal Box        
         # Blue color in BGR
-        color2 = (255, 0, 0)
-            
-        cv2.rectangle(frame, start_point2, end_point2, color2, self.thickness)
+        color2 = (255, 0, 0)            
+        cv2.rectangle(frame, self.start_point_pitch, self.end_point_pitch, color2, self.thickness)
 
         return frame
-      
+
+    def getVolPitchSection(self, frame):
+        volSection = frame[self.start_point_vol[1]:self.end_point_vol[1], self.start_point_vol[0]:self.end_point_vol[0]]
+        pitchSection = frame[self.start_point_pitch[1]:self.end_point_pitch[1], self.start_point_pitch[0]:self.end_point_pitch[0]]
+
+        return volSection, pitchSection
 
    
